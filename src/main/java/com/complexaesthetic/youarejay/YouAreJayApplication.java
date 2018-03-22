ff53a5a254fc3094ce6e822435aa46fb2f5fefa4
vvmk <vincentmasiello@gmail.com>
2018-03-22 18:49:58 -0400

    initial commit

---
+++ src/main/java/com/complexaesthetic/youarejay/YouAreJayApplication.java
@@ -0,0 +1,12 @@
+package com.complexaesthetic.youarejay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAreJayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAreJayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/complexaesthetic/youarejay/YouAreJayApplicationTest.java
@@ -0,0 +1,10 @@
+package com.complexaesthetic.youarejay;
+
+/**
+ * filename:
+ * project: YouAreJay
+ * author: https://github.com/vvmk
+ * date: 3/22/18
+ */
+public class YouAreJayApplicationTest {
+}
