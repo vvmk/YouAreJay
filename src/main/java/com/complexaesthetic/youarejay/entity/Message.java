2f0db60af47eeb1403c436162d308cabbdff3d80
vvmk <vincentmasiello@gmail.com>
2018-03-22 23:11:36 -0400

    Set up User and Message entities

---
+++ src/main/java/com/complexaesthetic/youarejay/entity/Message.java
@@ -0,0 +1,78 @@
+package com.complexaesthetic.youarejay.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * filename:
+ * project: YouAreJay
+ * author: https://github.com/vvmk
+ * date: 3/22/18
+ */
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue
+    private Long messageId;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User creator;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "recipientId")
+    private User recipient;
+
+    private String message;
+
+    public Message() {
+        // -_-
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/complexaesthetic/youarejay/entity/User.java
@@ -0,0 +1,78 @@
+package com.complexaesthetic.youarejay.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * filename:
+ * project: YouAreJay
+ * author: https://github.com/vvmk
+ * date: 3/22/18
+ */
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long userId;
+
+    private String name;
+
+    @CreatedDate
+    @Temporal(TemporalType.DATE)
+    private Date joinedOn;
+
+    private String githubId;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "recipientId")
+    private List<Message> receivedMessages;
+
+    public User() {
+        // -_-
+    }
+
+    public Date getJoinedOn() {
+        return joinedOn;
+    }
+
+    public void setJoinedOn(Date joinedOn) {
+        this.joinedOn = joinedOn;
+    }
+
+    public List<Message> getReceivedMessages() {
+        return receivedMessages;
+    }
+
+    public void setReceivedMessages(List<Message> receivedMessages) {
+        this.receivedMessages = receivedMessages;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithubId() {
+        return githubId;
+    }
+
+    public void setGithubId(String githubId) {
+        this.githubId = githubId;
+    }
+}
